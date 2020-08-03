0cd4f58fd93f513a0e535cc9693238807857a3b6
Vladislav Gryadko <zynga@ukr.net>
2020-08-03 19:37:25 +0300

    js код с логикой кнопки скролла наверх

---
+++ scripts/scrollup.js
@@ -0,0 +1,23 @@
+const scrollUp = document.querySelector(".scroll-up");
+
+
+window.addEventListener("scroll", (e) => {
+   e.preventDefault();
+
+   if (window.pageYOffset > 100) {
+      scrollUp.classList.add("opacity-on");
+      scrollUp.classList.remove("opacity-off");
+   } else {
+      scrollUp.classList.add("opacity-off");
+      scrollUp.classList.remove("opacity-on");
+   }
+});
+
+scrollUp.addEventListener("click", (e) => {
+   e.preventDefault();
+
+   window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+   });
+});
