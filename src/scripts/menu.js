79756b11bad747a2524d0aae9b86699ffb9cbdcc
Vladislav Gryadko <vgryadko@gmail.com>
2021-01-03 20:48:28 +0200

    rework header menu script

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,14 +1,8 @@
-const header = document.querySelector(".header");
+"use strict";
 
-header.classList.remove("header--no-js");
+const $ = require("jquery");
 
-const headerBtn = header.querySelector(".header__btn-menu");
-const headerList = header.querySelector(".header__list");
-const headerNav = header.querySelector(".header__navigation");
-
-headerBtn.addEventListener("click", (e) => {
-   e.preventDefault();
-   headerBtn.classList.toggle("header__btn-menu--open");
-   headerList.classList.toggle("header__list--closed");
-   headerNav.classList.toggle("header__navigation--page-down");
+$(".header__btn-menu").click(() => {
+   $(".header__btn-menu").toggleClass("header__btn-menu--open");
+   $(".header__list").slideToggle("slow").toggleClass("header__list--closed");
 });
