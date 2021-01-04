272d1c9da6f52d9c0f0d9f43c9a50f0f7bb5f634
Vladislav Gryadko <vgryadko@gmail.com>
2021-01-04 17:15:07 +0200

    add new styles, fonts, blocks for project

--- src/scripts/createPortfolioCards.js
+++ src/scripts/createPortfolioCards.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const $ = require("jquery");
-
 function createPortfolioCards(cards) {
    const portfolioList = document.querySelector(".portfolio__list");
    const portfolioTemplate = document.querySelector("#portfolio-item").content;
@@ -12,7 +10,7 @@ function createPortfolioCards(cards) {
 
    portfolioSource.srcset = cards.webp;
    portfolioPhoto.src = cards.photo;
-   portfolioPhoto.alt = "A photo of the " + cards.name + " site";
+   portfolioPhoto.alt = "Photo of the " + cards.name + " site";
    portfolioName.textContent = cards.name;
    portfolioLink.href = cards.link;
    const copyContent = portfolioTemplate.cloneNode(true);
@@ -21,100 +19,100 @@ function createPortfolioCards(cards) {
 
 const portfolioCards = [{
       name: "Alexis",
-      image: "images/alexis.png",
-      webp: "images/alexis.webp",
-      link: "portfolio-sites/alexis/"
+      image: "/images/alexis.png",
+      webp: "/images/alexis.png",
+      link: "/portfolio-sites/alexis/"
    },
 
    {
       name: "Pulse",
-      image: "images/pulse.png",
-      webp: "images/pulse.webp",
-      link: "portfolio-sites/pulse/"
+      image: "/images/pulse.png",
+      webp: "/images/pulse.webp",
+      link: "/portfolio-sites/pulse/"
    },
 
    {
       name: "Sedona",
-      image: "images/sedona.png",
-      webp: "images/sedona.webp",
-      link: "portfolio-sites/sedona/"
+      image: "/images/sedona.png",
+      webp: "/images/sedona.webp",
+      link: "/portfolio-sites/sedona/"
    },
 
    {
       name: "Mogo",
-      image: "images/mogo.png",
-      webp: "images/mogo.webp",
-      link: "portfolio-sites/mogo/"
+      image: "/images/mogo.png",
+      webp: "/images/mogo.webp",
+      link: "/portfolio-sites/mogo/"
    },
 
    {
       name: "Cat Energy",
-      image: "images/cat-energy.png",
-      webp: "images/cat-energy.webp",
-      link: "portfolio-sites/cat-energy/"
+      image: "/images/cat-energy.png",
+      webp: "/images/cat-energy.webp",
+      link: "/portfolio-sites/cat-energy/"
    },
 
    {
       name: "Gllacy",
-      image: "images/gllacy.png",
-      webp: "images/gllacy.webp",
-      link: "portfolio-sites/gllacy/"
+      image: "/images/gllacy.png",
+      webp: "/images/gllacy.webp",
+      link: "/portfolio-sites/gllacy/"
    },
 
    {
       name: "Misocial",
-      image: "images/misocial.png",
-      webp: "images/misocial.webp",
-      link: "portfolio-sites/misocial/"
+      image: "/images/misocial.png",
+      webp: "/images/misocial.webp",
+      link: "/portfolio-sites/misocial/"
    },
 
    {
       name: "Nerds",
-      image: "images/nerds.png",
-      webp: "images/nerds.webp",
-      link: "portfolio-sites/nerds/"
+      image: "/images/nerds.png",
+      webp: "/images/nerds.webp",
+      link: "/portfolio-sites/nerds/"
    },
 
    {
       name: "Borodinski",
-      image: "images/borodinski.png",
-      webp: "images/borodinski.webp",
-      link: "portfolio-sites/borodinski/"
+      image: "/images/borodinski.png",
+      webp: "/images/borodinski.webp",
+      link: "/portfolio-sites/borodinski/"
    },
 
    {
       name: "Covid",
-      image: "images/covid.png",
-      webp: "images/covid.webp",
-      link: "portfolio-sites/covid/"
+      image: "/images/covid.png",
+      webp: "/images/covid.webp",
+      link: "/portfolio-sites/covid/"
    },
 
    {
       name: "Project One",
-      image: "images/project-one.png",
-      webp: "images/project-one.webp",
-      link: "portfolio-sites/project-one/"
+      image: "/images/project-one.png",
+      webp: "/images/project-one.webp",
+      link: "/portfolio-sites/project-one/"
    },
 
    {
       name: "Styles Conference",
-      image: "images/styles-conference.png",
-      webp: "images/styles-conference.webp",
-      link: "portfolio-sites/styles-conference/"
+      image: "/images/styles-conference.png",
+      webp: "/images/styles-conference.webp",
+      link: "/portfolio-sites/styles-conference/"
    },
 
    {
       name: "Mycompany",
-      image: "images/mycompany.png",
-      webp: "images/mycompany.webp",
-      link: "portfolio-sites/mycompany/"
+      image: "/images/mycompany.png",
+      webp: "/images/mycompany.webp",
+      link: "/portfolio-sites/mycompany/"
    },
 
    {
       name: "Kvast",
-      image: "images/kvast.png",
-      webp: "images/kvast.webp",
-      link: "portfolio-sites/kvast/"
+      image: "/images/kvast.png",
+      webp: "/images/kvast.webp",
+      link: "/portfolio-sites/kvast/"
    },
 ];
 
